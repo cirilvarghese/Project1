2c00a4f7efc0d4614cc0694e57b9d55b616fa6bb
anagh534 <anaghkrkkl@gmail.com>
2023-11-08 19:34:36 +0530

    form prevented

--- script.js
+++ script.js
@@ -9,20 +9,24 @@ $(".header-button").mouseleave(function () {
     $(this).css({ "border-radius": "0px", "transition": "0.9s all" })
 })
 
-function send(e) {
-    event.preventDefault(e)
+function send() {
     var name = document.getElementById('name').value
     var email = document.getElementById('email').value
     var contact = document.getElementById('contact').value
-    const msg = document.getElementById('msg').value
+    var msg = document.getElementById('msg')
 
-    if (name.value === '' || email.value === '' || contact.value === '') {
+    if (name === '' || email === '' || contact === '') {
         msg.style.display = 'block';
+
+        return false
+
+    } else {
+        return true
     }
 
 }
 
-$('.section-3-button').mouseenter(function(){
-    $('.arrow').css({"display":"block", "transition":"0.9s"})
-    $('.section-3-button').text.css({"display":"none"})
+$('.section-3-button').mouseenter(function () {
+    $('.arrow').css({ "display": "block", "transition": "0.9s" })
+    $('.section-3-button').text.css({ "display": "none" })
 })
